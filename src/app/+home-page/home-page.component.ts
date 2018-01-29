2f7898d41997c6d8a2a171b7b9310cf4c730dedf
Giuseppe Digilio <giuseppe.digilio@4science.it>
2018-01-29 19:45:13 +0100

    Added tests to verify issue with patch cached response

--- src/app/+home-page/home-page.component.ts
+++ src/app/+home-page/home-page.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { WorkspaceitemDataService } from '../core/submission/workspaceitem-data.service';
+import { CollectionDataService } from '../core/data/collection-data.service';
+import { SubmissionRestService } from '../submission/submission-rest.service';
+import { Workspaceitem } from '../core/submission/models/workspaceitem.model';
+import { isNotEmpty } from '../shared/empty.util';
 
 @Component({
   selector: 'ds-home-page',
@@ -7,10 +11,43 @@ import { WorkspaceitemDataService } from '../core/submission/workspaceitem-data.
   templateUrl: './home-page.component.html'
 })
 export class HomePageComponent {
-  constructor(protected workspaceitemDataService: WorkspaceitemDataService) {}
+  constructor(private service: SubmissionRestService) {
+  }
 
   ngOnInit() {
-    this.workspaceitemDataService.findAll()
-      .subscribe((result) => console.log(result));
+    this.service.postToEndpoint('workspaceitems', {})
+      .map((workspaceitems) => workspaceitems[0])
+      .subscribe((workspaceitems: Workspaceitem) => {
+        const id = workspaceitems.id;
+        let body = [{
+          op: 'add',
+          path: '/sections/traditionalpageone/dc.title',
+          value: [{
+            value:'test update 1'
+          }]
+        }];
+        this.service.patchToEndpoint('workspaceitems', body, id)
+          .filter((rw) => isNotEmpty(rw))
+          .take(1)
+          .map((rw) => rw[0])
+          .subscribe((rw) => {
+            console.log('update_1', rw.sections.traditionalpageone);
+            body = [{
+              op: 'add',
+              path: '/sections/traditionalpageone/dc.title',
+              value: [{
+                value:'test update 2'
+              }]
+            }];
+            setTimeout(() => {
+            this.service.patchToEndpoint('workspaceitems', body, id)
+              .filter((rrw) => isNotEmpty(rrw))
+              .take(1)
+              .map((rrw) => rrw[0])
+              .subscribe((rrw) => {
+                console.log('update_2', rrw.sections.traditionalpageone);
+              })}, 1000)
+          })
+      })
   }
 }
